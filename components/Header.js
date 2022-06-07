d1c572dc01863647f284f0c078969fe2541592b6
maxpres <maximilian.pres@web.de>
2022-06-07 20:21:38 +0200

    update

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
 import {
   SearchIcon,
@@ -7,12 +7,59 @@ import {
   UserCircleIcon,
   UserIcon,
 } from '@heroicons/react/solid';
+import 'react-date-range/dist/styles.css'; //main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
+import { useRouter } from 'next/dist/client/router';
 
-function Header() {
+function Header({ placeholder }) {
+  const [searchInput, setSearchInput] = useState('');
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [noOfGuests, setNoOfGuests] = useState(1);
+  const router = useRouter();
+
+  const handleSelect = (ranges) => {
+    console.log(ranges); //{selection: {…}}
+    //  '   selection:
+    //     endDate: Thu Jun 09 2022 00:00:00 GMT+0200 (Mitteleuropäische Sommerzeit) {}
+    //     key: "selection"
+    //     startDate: Tue Jun 07 2022 00:00:00 GMT+0200 (Mitteleuropäische Sommerzeit) {}
+    //     [[Prototype]]: Object
+    //     [[Prototype]]: Object'
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
+  const search = () => {
+    router.push({
+      pathname: '/search',
+      query: {
+        location: searchInput,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+        noOfGuests,
+      },
+    });
+  };
+
+  const resetInput = () => {
+    setSearchInput('');
+  };
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: 'selection',
+  };
+
   return (
     <header className='sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10'>
       {/* Left */}
-      <div className='relative flex items-center h-10 cursor-pointer my-auto'>
+      <div
+        onClick={() => router.push('/')}
+        className='relative flex items-center h-10 cursor-pointer my-auto'
+      >
         <Image
           src='https://links.papareact.com/qd3'
           layout='fill'
@@ -24,9 +71,11 @@ function Header() {
       {/* Middle */}
       <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
         <input
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
           className='text-gray-600 flex-grow pl-5 bg-transparent outline-none'
           type='text'
-          placeholder='Start your search'
+          placeholder={placeholder || 'Start your search'}
         />
         <SearchIcon className='hidden h-8 bg-red-400 text-white rounded-full p-1.5 cursor-pointer md:inline-flex md:mr-1' />
       </div>
@@ -40,6 +89,38 @@ function Header() {
           <UserCircleIcon className='h-6' />
         </div>
       </div>
+
+      {searchInput && (
+        <div className='flex flex-col col-span-3 mx-auto'>
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDay={new Date()}
+            rangeColors={['#FD5B61']}
+            onChange={handleSelect}
+          />
+          <div className='flex items-center border-b mb-4'>
+            <h2 className='text-2xl flex-grow font-semibold'>
+              Number of guests
+            </h2>
+            <UserIcon className='h-5' />
+            <input
+              value={noOfGuests}
+              min={1}
+              onChange={(e) => setNoOfGuests(e.target.value)}
+              type='number'
+              className='w-12 pl-2 text-lg outline-none text-red-400'
+            />
+          </div>
+          <div className='flex'>
+            <button onClick={resetInput} className='flex-grow text-gray-500'>
+              Cancel
+            </button>
+            <button onClick={search} className='flex-grow text-red-400'>
+              Search
+            </button>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
